261a1d9265a6e7c6afba494c7cc6ea6afa9078c5
Ville Peurala <ville.peurala@gmail.com>
2017-11-14 22:33:09 +0200

    First piece of code.

--- main.rb
+++
@@ -1,9 +0,0 @@
-puts ARGV
-
-puts "COCKBOYS"
-
-kokonaisbruttosumma = ARGV[0]
-
-puts "kokonaisbruttosumma: #{kokonaisbruttosumma}"
-
-# Pääomatulon veroprosentti vuonna 2017 on 30, 30 000 euroa ylittävältä osalta 34. [https://www.veronmaksajat.fi/Palkka-ja-elake/Veroprosentit/veroprosentit-2017/]
---
+++ paaomavero.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+# Pääomatulon veroprosentti vuonna 2017 on 30, 30 000 euroa ylittävältä osalta 34. [https://www.veronmaksajat.fi/Palkka-ja-elake/Veroprosentit/veroprosentit-2017/]
+
+usage = <<USAGE
+Käyttö::
+./paaomavero bruttotulo
+
+Parametrin "bruttotulo" tulee olla numero.
+
+Esimerkkejä:
+./paaomavero 50000
+./paaomavero 32556.63
+USAGE
+
+if ARGV.length +== 0
+  puts "Pakollinen syötearvo puuttuu."
+  puts
+  usage
+  exit 1
+elsif ARV.length > 1
+  puts "Liikaa syötearvoja."
+  puts
+  usage
+  exit 2
+end
+
+kokonaisbruttosumma = ARGV[0].to_i
+
+puts "Kokonaisbruttosumma: #{kokonaisbruttosumma}"
