87b0e00f40b6c406f9406cd5467283fd54d839bf
Ville Peurala <ville.peurala@gmail.com>
2017-11-14 23:09:30 +0200

    Some validation.

--- paaomavero.rb
+++ paaomavero.rb
@@ -2,7 +2,8 @@
 
 # Pääomatulon veroprosentti vuonna 2017 on 30, 30 000 euroa ylittävältä osalta 34. [https://www.veronmaksajat.fi/Palkka-ja-elake/Veroprosentit/veroprosentit-2017/]
 
-usage = <<USAGE
+def usage() 
+<<USAGE
 Käyttö::
 ./paaomavero bruttotulo
 
@@ -12,19 +13,27 @@ Esimerkkejä:
 ./paaomavero 50000
 ./paaomavero 32556.63
 USAGE
+end
 
-if ARGV.length !== 0
-  puts "Pakollinen syötearvo puuttuu."
-  puts
-  usage
-  exit 1
-elsif ARGV.length > 1
-  puts "Liikaa syötearvoja."
-  puts
-  usage
-  exit 2
+if ARGV.length == 0 then
+        puts "Pakollinen syötearvo puuttuu."
+        puts
+        puts usage
+        exit 1
+elsif ARGV.length > 1 then 
+	puts "Liikaa syötearvoja."
+        puts
+        puts usage
+        exit 2
 end
 
 kokonaisbruttosumma = ARGV[0].to_i
+
+if kokonaisbruttosumma.to_s != ARGV[0] then
+	puts "Parametri \"bruttotulo\" ei ole validi numero: #{ARGV[0]}."
+	puts
+	puts usage
+	exit 3
+end
 
 puts "Kokonaisbruttosumma: #{kokonaisbruttosumma}"
